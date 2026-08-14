d7b1f88ddb0245036ad2bc99c6aa619385650fb1
Sahil Khatri <srkkhatri09@gmail.com>
2026-08-14 23:22:37 +0530

    structure of scrapping

---
+++ Source/scrap.py
@@ -0,0 +1,6 @@
+# Linkden, Wellfound, Cutshort, Hirist are the platform for the scrapping
+# i will scrap data of the 2026 atmost 500 jobs hiring in ML/AI
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np
+
